f3b28f16274f8953ddd1f134cab88246a422d33f
todor-enikov <enigmaenikov@gmail.com>
2016-11-01 23:02:03 +0200

    Committed homework variables data expressions.

---
+++ High Quality Code - Part 1 - 2016/05. VariablesDataExpressions/VariablesDataExpressionsAndConstants/ClassSizeInCSharp.cs
@@ -0,0 +1,68 @@
+﻿namespace VariablesDataExpressionsAndConstants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ClassSizeInCSharp
+    {
+        private double width;
+        private double height;
+
+        public ClassSizeInCSharp(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new IndexOutOfRangeException("The width cannot be less or equal to zero!");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new IndexOutOfRangeException("The height cannot be less or equal to zero!");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public static ClassSizeInCSharp GetRotatedSize(ClassSizeInCSharp size, double angleOfTheFigure)
+        {
+            double widthOfTheFigure = size.Width;
+            double heightOfTheFigure = size.Height;
+            double cosine = Math.Abs(Math.Cos(angleOfTheFigure));
+            double sinus = Math.Abs(Math.Sin(angleOfTheFigure));
+            var result = new ClassSizeInCSharp(
+                                              (cosine * widthOfTheFigure) + (sinus * heightOfTheFigure),
+                                              (sinus * widthOfTheFigure) + (cosine * heightOfTheFigure));
+            return result;
+        }
+    }
+}
---
+++ High Quality Code - Part 1 - 2016/05. VariablesDataExpressions/VariablesDataExpressionsAndConstants/MethodPrintStatisticsInCSharp.cs
@@ -0,0 +1,67 @@
+﻿namespace VariablesDataExpressionsAndConstants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MethodPrintStatisticsInCSharp
+    {
+        public static void Main()
+        {
+            double[] array = { 1, 2, 3, 4, 5 };
+            int counter = 5;
+            MethodPrintStatisticsInCSharp.PrintStatistics(array, counter);
+        }
+
+        public static void PrintStatistics(double[] elements, int count)
+        {
+            double maxElement = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (elements[i] > maxElement)
+                {
+                    maxElement = elements[i];
+                }
+            }
+
+            PrintTheMaximumElement(maxElement);
+
+            double minElement = elements[0];
+            for (int i = 0; i < count; i++)
+            {
+                if (elements[i] < minElement)
+                {
+                    minElement = elements[i];
+                }
+            }
+
+            PrintTheMinimumElement(minElement);
+
+            double value = 0;
+            for (int i = 0; i < count; i++)
+            {
+                value += elements[i];
+            }
+
+            double average = value / count;
+            PrintAverage(average);
+        }
+
+        public static void PrintAverage(double average)
+        {
+            Console.WriteLine("The average in the array is: {0}", average);
+        }
+
+        private static void PrintTheMinimumElement(double minElement)
+        {
+            Console.WriteLine("The minimum element in the array is: {0}", minElement);
+        }
+
+        private static void PrintTheMaximumElement(double maxElement)
+        {
+            Console.WriteLine("The maximum element in the array is: {0}", maxElement);
+        }
+    }
+}
