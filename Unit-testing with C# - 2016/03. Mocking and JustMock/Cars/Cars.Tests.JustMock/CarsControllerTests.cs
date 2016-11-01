bd5ec15858e5537807a2e4640a29d3075d4f5a04
todor-enikov <enigmaenikov@gmail.com>
2016-11-01 23:06:30 +0200

    Committed homework mocking and just mock.

---
+++ Unit-testing with C# - 2016/03. Mocking and JustMock/Cars/Cars.Tests.JustMock/CarsControllerTests.cs
@@ -0,0 +1,207 @@
+﻿namespace Cars.Tests.JustMock
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Cars.Contracts;
+    using Cars.Tests.JustMock.Mocks;
+    using Cars.Controllers;
+    using Cars.Models;
+    using Telerik.JustMock;
+    using Moq;
+    using Cars.Data;
+
+    [TestClass]
+    public class CarsControllerTests
+    {
+        private readonly ICarsRepository carsData;
+        private CarsController controller;
+
+        public CarsControllerTests()
+            : this(new JustMockCarsRepository())
+        // : this(new MoqCarsRepository())
+        {
+        }
+
+        private CarsControllerTests(ICarsRepositoryMock carsDataMock)
+        {
+            this.carsData = carsDataMock.CarsData;
+        }
+
+        [TestInitialize]
+        public void CreateController()
+        {
+            this.controller = new CarsController(this.carsData);
+        }
+
+        [TestMethod]
+        public void IndexShouldReturnAllCars()
+        {
+            var model = (ICollection<Car>)this.GetModel(() => this.controller.Index());
+
+            Assert.AreEqual(4, model.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarIsNull()
+        {
+            var model = (Car)this.GetModel(() => this.controller.Add(null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarMakeIsNull()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "",
+                Model = "330d",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarModelIsNull()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "BMW",
+                Model = "",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+        }
+
+        [TestMethod]
+        public void AddingCarShouldReturnADetail()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "BMW",
+                Model = "330d",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+
+            Assert.AreEqual(1, model.Id);
+            Assert.AreEqual("Audi", model.Make);
+            Assert.AreEqual("A5", model.Model);
+            Assert.AreEqual(2005, model.Year);
+        }
+
+        [TestMethod]
+        public void Search_ShouldSearchForACar_ByGivenString()
+        {
+            // Arrange
+            var mock = new Mock<ICarsRepository>();
+            mock.Setup(m => m.Search(It.IsAny<string>())).Returns(new List<Car>());
+            var fakeCar = mock.Object;
+            var controller = new CarsController(fakeCar);
+
+            // Act
+            var result = controller.Search(string.Empty);
+
+            // Assert
+            mock.Verify(m => m.Search(It.IsAny<string>()), Times.Exactly(1));
+
+        }
+
+        [TestMethod]
+        public void Sort_ShouldThrowException_IfStringIsNotCorrect()
+        {
+            // Arrange
+            bool actual = false;
+            var differentSortingParameter = "Kvo ne chu?!";
+            var controller = new CarsController();
+
+            // Act
+            try
+            {
+                var result = controller.Sort(differentSortingParameter);
+            }
+            catch (Exception ex)
+            {
+                actual = ex.Message.Contains("Invalid");
+            }
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Sort_ShouldCoverCaseMake_IfStringIsmake()
+        {
+            //Arrange
+            var mock = new Mock<ICarsRepository>();
+            mock.Setup(m => m.SortedByMake());
+            var fakeCarRepository = mock.Object;
+            var controller = new CarsController(fakeCarRepository);
+
+            // Act
+            var result = controller.Sort("make");
+
+            // Assert
+            mock.Verify(m => m.SortedByMake(), Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void Sort_ShouldCoverCaseYear_IfTheStringIsyear()
+        {
+            // Arrange
+            var mock = new Mock<ICarsRepository>();
+            mock.Setup(m => m.SortedByYear());
+            var fakeCarRepository = mock.Object;
+            var controller = new CarsController(fakeCarRepository);
+
+            // Act
+            var result = controller.Sort("year");
+
+            // Assert
+            mock.Verify(m => m.SortedByYear(), Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void Details_ShouldThrowException_IfCarIsNull()
+        {
+            // Arrange
+            Car car = null;
+
+            var mock = new Mock<ICarsRepository>();
+            mock.Setup(m => m.GetById(It.IsAny<int>())).Returns(car);
+
+            var fakeCarRepository = mock.Object;
+            var controller = new CarsController(fakeCarRepository);
+            var actual = false;
+
+            // Act
+            try
+            {
+                var result = controller.Details(5);
+            }
+            catch(ArgumentNullException ex)
+            {
+                actual = ex.Message.Contains("Car");
+            }
+
+            // Assert
+            Assert.IsTrue(actual);
+
+        }
+        private object GetModel(Func<IView> funcView)
+        {
+            var view = funcView();
+            return view.Model;
+        }
+    }
+}
---
+++ Unit-testing with C# - 2016/03. Mocking and JustMock/Cars/Cars.Tests.JustMock/CarsRepositoryTests.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using NUnit.Framework;
+using Cars.Models;
+using Cars.Data;
+using System.Collections.Generic;
+using Moq;
+using Cars.Tests.JustMock.Mocks;
+using Cars.Contracts;
+using System.Linq;
+
+namespace Cars.Tests.JustMock
+{
+    [TestFixture]
+    public class CarsRepositoryTests
+    {
+        [Test]
+        public void Add_ShouldAddCarToCarRepository()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var db = new Database();
+            db.Cars = new List<Car>();
+            var count = db.Cars.Count;
+            var carsRepository = new CarsRepository(db);
+
+            // Act
+            carsRepository.Add(car);
+
+            // Assert
+            Assert.AreEqual(1, db.Cars.Count);
+        }
+
+        [Test]
+        public void Add_ShouldThrowArgumentException_IfCarIsNull()
+        {
+            // Arrange
+            Car car = null;
+            var db = new Database();
+            db.Cars = new List<Car>();
+            var count = db.Cars.Count;
+            var carsRepository = new CarsRepository(db);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => carsRepository.Add(car), "Null car cannot be added");
+        }
+
+        [Test]
+        public void Remove_ShouldremoveCarFromCarsRepository_WhenIsCalled()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var db = new Database();
+            db.Cars = new List<Car>();
+            var carsRepository = new CarsRepository(db);
+
+            // Act
+            carsRepository.Add(car);
+            carsRepository.Remove(car);
+
+            // Assert
+            Assert.AreEqual(0, db.Cars.Count);
+        }
+
+        [Test]
+        public void Remove_ShouldThrowArgumentException_IfCarIsNull()
+        {
+            // Arrange
+            Car car = null;
+            var carsRepository = new CarsRepository();
+
+            // Act and Assert
+            var ex = Assert.Throws<ArgumentException>(() => carsRepository.Remove(car));
+            Assert.IsTrue(ex.Message.Contains("Null car cannot be removed"));
+        }
+
+        [Test]
+        public void GetById_ShouldFindCarById()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(new List<Car>() { });
+            var fakeDB = mock.Object;
+            fakeDB.Cars.Add(car);
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var result = carsRepository.GetById(1);
+
+            // Assert
+            mock.Verify(m => m.Cars, Times.Exactly(2));
+        }
+
+        [Test]
+        public void GetById_ShouldThrowException_IfThereIsNoCarWithThatId()
+        {
+            // Arrange
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(new List<Car>() { }); ;
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act and Assert
+            var ex = Assert.Throws<ArgumentException>(() => carsRepository.GetById(7));
+            Assert.IsTrue(ex.Message.Contains("Car with such Id could not be found"));
+        }
+
+        [Test]
+        public void All_ShouldReturnAllOfTheCarsInTheDB()
+        {
+            // Arrange
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var secondCar = new Car { Id = 2, Make = "Moskvich", Model = "12-tak", Year = 1980 };
+            var thirdCar = new Car { Id = 3, Make = "Lada", Model = "7-marka", Year = 1991 };
+
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(new List<Car>() { });
+            var fakeDB = mock.Object;
+            fakeDB.Cars.Add(firstCar);
+            fakeDB.Cars.Add(secondCar);
+            fakeDB.Cars.Add(thirdCar);
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var result = carsRepository.All();
+
+            //Assert
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [Test]
+        public void SortedByMake_ShouldReturnSortedListOfCarsByMake()
+        {
+            // Arrange
+            var datebase = new List<Car>();
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var secondCar = new Car { Id = 2, Make = "Moskvich", Model = "12-tak", Year = 1980 };
+            var thirdCar = new Car { Id = 3, Make = "Lada", Model = "7-marka", Year = 1991 };
+            datebase.Add(firstCar);
+            datebase.Add(secondCar);
+            datebase.Add(thirdCar);
+
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(datebase);
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var sortedListByMake = carsRepository.SortedByMake();
+            var expected = "Audi";
+
+            // Assert
+            Assert.AreEqual(expected, (sortedListByMake.First()).Make);
+        }
+
+        [Test]
+        public void SortedByYear_ShouldReturnSortedListOfCarsByYear()
+        {
+            // Arrange
+            var datebase = new List<Car>();
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var secondCar = new Car { Id = 2, Make = "Moskvich", Model = "12-tak", Year = 1980 };
+            var thirdCar = new Car { Id = 3, Make = "Lada", Model = "7-marka", Year = 1991 };
+            datebase.Add(firstCar);
+            datebase.Add(secondCar);
+            datebase.Add(thirdCar);
+
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(datebase);
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var sortedListByYear = carsRepository.SortedByYear();
+            var expected = 2005;
+
+            // Assert
+            Assert.AreEqual(expected, (sortedListByYear.First()).Year);
+        }
+
+        [Test]
+        public void Search_ShouldReturnCarByGivenString()
+        {
+            // Arrange
+            var datebase = new List<Car>();
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            datebase.Add(firstCar);
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(datebase);
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var result = carsRepository.Search("Audi");
+
+            // Assert
+            mock.Verify(m => m.Cars, Times.Exactly(1));
+        }
+
+        [Test]
+        public void Search_ShouldReturnListOfAllCars_IfStringIsNullOrEmpty()
+        {
+            // Arrange
+            var datebase = new List<Car>();
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            var secondCar = new Car { Id = 2, Make = "Moskvich", Model = "12-tak", Year = 1980 };
+            datebase.Add(firstCar);
+            datebase.Add(secondCar);
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(datebase);
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var result = carsRepository.Search("");
+
+            // Assert
+            mock.Verify(m => m.Cars, Times.Exactly(1));
+        }
+
+        [Test]
+        public void Testing_GetterOnClassCarsRepository()
+        {
+            // Arrange
+            var datebase = new List<Car>();
+            var firstCar = new Car { Id = 1, Make = "Audi", Model = "A5", Year = 2005 };
+            datebase.Add(firstCar);
+            var mock = new Mock<IDatabase>();
+            mock.Setup(m => m.Cars).Returns(datebase);
+            var fakeDB = mock.Object;
+            var carsRepository = new CarsRepository(fakeDB);
+
+            // Act
+            var totalCars = carsRepository.TotalCars;
+            var expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, totalCars);
+        }
+    }
+}
