9dfd4f5f13cc9535a4eb4e067fcb87622c4e7a7a
todor-enikov <enigmaenikov@gmail.com>
2016-11-01 23:15:23 +0200

    Committed homework HTML templates.

---
+++ JavaScript UI и DOM - 2016/04. HTML Templates/Task 3 - ListView/task3.js
@@ -0,0 +1,26 @@
+function solve() {
+    return function () {
+        $.fn.listview = function (data) {
+            var element,
+                templateSourceID,
+                source,
+                template,
+                length = data.length,
+                item;
+
+            element = $(this);
+            templateSourceID = element.attr('data-template');
+            source = $('#' + templateSourceID).html();
+            template = handlebars.compile(source); // LOWER H ON HANDLEBARS !!!!!!!!!!!!!!!!!!!!!
+
+            for (item = 0; item < length; item += 1) {
+                element.append(template(data[item]));
+            }
+
+            return this;
+        };
+    };
+}
+
+var html = document.getElementById('information').innerHTML;
+var informationTemplate = Handlebars.compile(html);
