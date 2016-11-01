6a6dc21dd0f548f6471b0ed556a4c6c8e29e76b3
todor-enikov <enigmaenikov@gmail.com>
2016-11-01 22:58:52 +0200

    Committed homework regular expressions.

---
+++ Javascript/12. Regular Expressions/1. Format with placeholders.js
@@ -0,0 +1,19 @@
+
+function solve(args) {
+    const options = JSON.parse(args[0]);
+
+    let inputString = args[1] + '',
+        matchProps = /#\{(.*?) \}/g;
+
+    for (let item in options) {
+        inputString = inputString
+            .replace(new RegExp(`#{${item}}`, 'g'), options[item]);
+
+    }
+
+    console.log(inputString);
+}
+
+module.exports = {
+    solve
+};
