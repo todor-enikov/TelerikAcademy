bb81b447ffcf56aaf3307e85c5ecf919832b9efe
todor-enikov <enigmaenikov@gmail.com>
2016-11-01 23:07:49 +0200

    Committed homework defensive programming and exceptions.

---
+++ High Quality Code - Part 2 - 2016/01. Defensive-Programming-and-Exceptions/Exceptions-Homework/ExamResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class ExamResult
+{
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        if (grade < 0)
+        {
+            throw new ArgumentException("Grade should not be less than zero.");
+        }
+
+        if (minGrade < 0)
+        {
+            throw new ArgumentException("minGrade should not be less than zero.");            
+        }
+
+        if (maxGrade <= minGrade)
+        {
+            throw new ArithmeticException("minGrade cannot be bigger than maxGrade.");
+        }
+
+        if (comments == null || comments == string.Empty)
+        {
+            throw new ArgumentNullException("Comments cannot be null or empty");
+        }
+
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade { get; private set; }
+
+    public int MinGrade { get; private set; }
+
+    public int MaxGrade { get; private set; }
+
+    public string Comments { get; private set; }
+}
---
+++ High Quality Code - Part 2 - 2016/01. Defensive-Programming-and-Exceptions/Exceptions-Homework/SimpleMathExam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class SimpleMathExam : Exam
+{
+    public SimpleMathExam(int problemsSolved)
+    {
+        var isValidNumberOfProblems = 0 <= problemsSolved && problemsSolved <= 2;
+        if (!isValidNumberOfProblems)
+        {
+            throw new ArgumentOutOfRangeException("Invalid number of problems.");
+        }
+
+        this.ProblemsSolved = problemsSolved;
+    }
+
+    public int ProblemsSolved { get; private set; }
+
+    public override ExamResult Check()
+    {
+        if (this.ProblemsSolved == 0)
+        {
+            return new ExamResult(2, 2, 6, "Bad result: nothing done.");
+        }
+        else if (this.ProblemsSolved == 1)
+        {
+            return new ExamResult(4, 2, 6, "Average result: nothing done.");
+        }
+        else if (this.ProblemsSolved == 2)
+        {
+            return new ExamResult(6, 2, 6, "Average result: nothing done.");
+        }
+
+        return new ExamResult(0, 0, 0, "Invalid number of problems solved!");
+    }
+}
